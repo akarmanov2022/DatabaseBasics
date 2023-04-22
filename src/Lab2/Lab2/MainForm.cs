0b65b020f154c6b4af4a3978ac7a667ad75e4b60
Arseniy Karmanov <arseniy.karmanov@mail.ru>
2023-04-22 18:53:30 +0700

    Initial DB connect.

--- src/Lab2/Lab2/MainForm.cs
+++ src/Lab2/Lab2/MainForm.cs
@@ -10,9 +10,10 @@ using System.Windows.Forms;
 
 namespace Lab2
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+
+        public MainForm()
         {
             InitializeComponent();
         }
